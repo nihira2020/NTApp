989296d227e845f00ce0e409e859c3e3da5ecc1a
Natarajan <nataraj.itsolution@gmail.com>
2024-05-04 08:17:23 +0530

    slider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { authGuard } from './_service/auth.guard';
+import { SliderComponent } from './component/slider/slider.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -15,5 +16,8 @@ export const routes: Routes = [
     {
         path: 'customer/edit/:code', loadComponent: () => import('./component/addcustomer/addcustomer.component').then(m => m.AddcustomerComponent),
         canActivate: [authGuard]
+    },
+    {
+        path:'slider',component:SliderComponent
     }
 ];
---
+++ src/app/component/slider/slider.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider.component.html',
+  styleUrl: './slider.component.css'
+})
+export class SliderComponent {
+
+}
